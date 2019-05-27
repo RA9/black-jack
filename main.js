24569b6d8793ea0c6cc1ff2e08b8dff269cf790f
Carlos Nah <cnah27@gmail.com>
2019-05-27 09:25:01 +0000

    MAJOR v0.1.0: Initialize Application Scaffold

---
+++ main.js
@@ -0,0 +1,13 @@
+/* 
+  @author: Carlos S. Nah
+  @title: Black Jack Game
+  @version: v0.1.0
+  @date: 05/26/19
+ */
+ 
+ let cards = ["Queen", "Heart"]
+ 
+ console.log("Welcome to the world of Black Jack a game that amuse the auidence with much fun.")
+ 
+ console.log(cards[0])
+ console.log(cards[1])
