037f0d6e36276606ea1d466572ba9ade231b337a
Carlos Nah <cnah27@gmail.com>
2019-05-28 23:37:33 +0000

    MINOR v0.1.2: Store all cards in array

--- main.js
+++ main.js
@@ -6,8 +6,11 @@
  */
  
  let cards = ["Queen", "Heart"]
- let playerCards = []
+ let playerCards = [] // Players card array
+ let deck = []
+ let suits = ["Hearts", "Spades", "Daimonds", "Clubs"]
  
+ let values = ["King", "Queen", "Jack", "Ace","Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
  console.log(playerCards)
  
  console.log("Welcome to the world of Black Jack a game that amuse the auidence with much fun.")
