015a75404125715b22a8696fcafd2098753bc656
Carlos Nah <cnah27@gmail.com>
2019-05-29 00:18:40 +0000

    PATCH v0.1.3: Add functions to the game

--- main.js
+++ main.js
@@ -5,12 +5,38 @@
   @date: 05/26/19
  */
  
- let cards = ["Queen", "Heart"]
+ let cards = []
  let playerCards = [] // Players card array
  let deck = []
  let suits = ["Hearts", "Spades", "Daimonds", "Clubs"]
  
  let values = ["King", "Queen", "Jack", "Ace","Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+ 
+ for(let i = 0; i <= 52; i++) {
+   cards[i] = Math.random(suits[i], values[i])
+ } 
+ 
+ // Define a function to create the cards deck
+ function createCardDeck(){
+   for(let i = 0; i <= cards.length; i++) {
+     deck.push(i)
+   }
+   return deck
+ }
+ 
+ // Define a function to select a card from the top of the deck
+ function selectTopCard(){
+   let card = creatCardDeck()
+   let randomNum = Math.random(1, 52)
+   let topCard = 0
+   if(!card[-1]) {
+    topCard += card[randomNum]
+   }
+   return topCard
+ }
+ 
+ playerCards.push(selectTopCard)
+ playerCards.push(selectTopCard)
  console.log(playerCards)
  
  console.log("Welcome to the world of Black Jack a game that amuse the auidence with much fun.")
