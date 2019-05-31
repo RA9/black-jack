78bc6fcaa5e8b5bdabd2416b26274191a5d34509
Carlos Nah <cnah27@gmail.com>
2019-05-31 00:34:44 +0000

    PATCH v0.1.3: Add functions to the game

--- main.js
+++ main.js
@@ -5,38 +5,34 @@
   @date: 05/26/19
  */
  
- let cards = []
- let playerCards = [] // Players card array
+ 
  let deck = []
  let suits = ["Hearts", "Spades", "Daimonds", "Clubs"]
  
  let values = ["King", "Queen", "Jack", "Ace","Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
  
- for(let i = 0; i <= 52; i++) {
-   cards[i] = Math.random(suits[i], values[i])
- } 
  
  // Define a function to create the cards deck
  function createCardDeck(){
-   for(let i = 0; i <= cards.length; i++) {
-     deck.push(i)
-   }
-   return deck
+   let cardDeck = [];
+   for(let i = 0; i < suits.length; i++) {
+   for(let j = 0; j < values.length; j++) {
+    let card = {
+      suit: suits[i],
+      value: values[j]
+    }
+    cardDeck.push(card)
+   } 
+ } 
+ return deck;
  }
- 
+ let deck = createCardDeck();
  // Define a function to select a card from the top of the deck
  function selectTopCard(){
-   let card = creatCardDeck()
-   let randomNum = Math.random(1, 52)
-   let topCard = 0
-   if(!card[-1]) {
-    topCard += card[randomNum]
-   }
-   return topCard
+   return deck.shift()
  }
  
- playerCards.push(selectTopCard)
- playerCards.push(selectTopCard)
+let playerCards = [selectTopCard(),selectTopCard()]// Players card array
  console.log(playerCards)
  
  console.log("Welcome to the world of Black Jack a game that amuse the auidence with much fun.")
